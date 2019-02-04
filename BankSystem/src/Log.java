4ebb1c3f73105f5da4cabf428e70934a15274318
Ofir Blumental <46105683+webko123@users.noreply.github.com>
2019-02-05 00:14:06 +0200

    Update Log.java

--- BankSystem/src/Log.java
+++ BankSystem/src/Log.java
@@ -1,28 +1,17 @@
 
-
 public class Log {
 
-	long timestamp;
-	int clientid;
-	String description;
-	float amount;
-	String data;
+	private long timestamp;
+	private int clientid;
+	private String description;
+	private float amount;
 
 	public Log(long timestamp, int clientid, String description, float amount) {
 		super();
 		setTimestamp(timestamp);
-		setAmount(amount);
 		setClientid(clientid);
 		setDescription(description);
-
-	}
-	
-	public String getData() {
-		return null;
-	}
-	
-	public void setData(String Data) {
-		this.data = data;
+		setAmount(amount);
 	}
 
 	public long getTimestamp() {
@@ -56,15 +45,49 @@ public class Log {
 	public void setAmount(float amount) {
 		this.amount = amount;
 	}
+	
+	public String getData() {
+		return String.format("Client %d just %s with the amount of %f, it happend at %d",clientid,description,amount,timestamp );
+	}
 
 	@Override
 	public String toString() {
 		return "Log [timestamp=" + timestamp + ", clientid=" + clientid + ", description=" + description + ", amount="
-				+ amount + ", data=" + data + ", getData()=" + getData() + ", getTimestamp()=" + getTimestamp()
-				+ ", getClientid()=" + getClientid() + ", getDescription()=" + getDescription() + ", getAmount()="
-				+ getAmount() + "]";
+				+ amount + "]";
 	}
 
-	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(amount);
+		result = prime * result + clientid;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + (int) (timestamp ^ (timestamp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Log other = (Log) obj;
+		if (Float.floatToIntBits(amount) != Float.floatToIntBits(other.amount))
+			return false;
+		if (clientid != other.clientid)
+			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (timestamp != other.timestamp)
+			return false;
+		return true;
+	}
 
 }
